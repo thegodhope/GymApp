92bac76f8e3d0e1a87c79417a95652530e03e73d
thegodhope <103210115+thegodhope@users.noreply.github.com>
2024-01-22 23:26:30 +0100

    root dir issue fix

--- gymappapis/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const router = Router();
-
-const { createUser, login } = require("../controllers/userController");
-
-router.post("/signUp", createUser);
-router.post("/login", login);
-
-module.exports = router;
